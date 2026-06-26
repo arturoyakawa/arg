98ab85f39f080890ceeda706c4e7e9d5ea3d7b10
arturoyakawa <arturoyakawa5@gmail.com>
2026-06-25 22:56:53 -0300

    se

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Init")
+console.log("don't look over here, @pacni7")
+/** @type {HTMLAudioElement} */
+const audio = document.getElementById("fallenchild");
+
+audio.volume = .2
+audio.addEventListener("canplaythrough", () => {
+    audio.play();
+}, { once: true });
