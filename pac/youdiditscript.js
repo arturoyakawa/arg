8c07d9f236dca32a18ff401e0872b5d1f541fed8
arturoyakawa <arturoyakawa5@gmail.com>
2026-06-27 02:40:17 -0300

    audio fah

--- pac/youdiditscript.js
+++ pac/youdiditscript.js
@@ -1,9 +1,16 @@
+/** @type {HTMLAudioElement} */
 const bgm = document.getElementById("bgm")
+bgm.loop = true
+
 let clickedOnce = false
-window.addEventListener("mousedown", () => {
-console.log("AJSUDAJUDAJUDASJD")
-     if (clickedOnce) { return }
-    console.log("click")
+document.addEventListener("selectionchange", () => {
+    if (clickedOnce) { return }
+
+    const selection = window.getSelection();
+
+    if (!(selection.toString().length > 40)) {
+        return
+    }
     document.body.classList.add('snow')
     bgm.volume = .2
     bgm.play()
